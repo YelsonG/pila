a50fce60fcebd84bd13014477759bf0eb52792da
YelsonG <125761141+YelsonG@users.noreply.github.com>
2023-07-22 18:37:38 -0600

    Add files via upload

---
+++ Pilas.py
@@ -0,0 +1,93 @@
+class Stack:
+    
+   
+def __init__(self):
+        self.items = []
+
+    
+        self.items = []
+
+   
+
+        self.items = []
+
+
+       
+def is_empty(self):
+        
+       
+return len(self.items) == 0
+
+    
+
+   
+
+
+def push(self, item):
+        self.items.append(item)
+
+    
+        self.items.append(item)
+
+   
+
+        self.items.append(item)
+
+
+       
+def pop(self):
+        if not self.is_empty():
+            
+           
+return self.items.pop()
+        else:
+            
+           
+print("La pila está vacía.")
+            
+           
+return None
+
+    def peek(self):
+        
+       
+if not self.is_empty():
+            
+           
+return self.items[-1]
+        else:
+            
+           
+print("La pila está vacía.")
+            return None
+
+# Ejemplo de uso:
+stack = Stack()
+
+
+stack = Stack
+# Agregar elementos a la pila
+stack.push(5)
+stack.push(
+stack
+10)
+stack.push(15)
+
+# Verificar si la pila está vacía
+print("¿La pila está vacía?", stack.is_empty())  # Salida: ¿La pila está vacía? False
+
+# Obtener el elemento del tope de la pila sin removerlo
+print("Elemento en la cima de la pila:", stack.peek())  # Salida: Elemento en la cima de la pila: 15
+
+# Remover y retornar el elemento del tope de la pila
+popped_item = stack.pop()
+
+popped_item = stack
+
+popped_item =
+
+popped_item
+print("Elemento eliminado:", popped_item)  # Salida: Elemento eliminado: 15
+
+# Verificar si la pila está vacía después de hacer pop
+print("¿La pila está vacía después de pop?", stack.is_empty())  # Salida: ¿La pila está vacía después de pop? False
